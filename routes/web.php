befbb005867a376608e4489469e88752ecc25f13
baqueroglenncarlo <baqueroglenncarlo@gmail.com>
2026-01-17 11:30:06 +0800

    #3 - Implement data refresh in dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\API\JokeController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', [JokeController::class, 'index']);
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::get('/dashboard',[JokeController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/dashboard',[JokeController::class, 'index'])->name('dashboard');
+
+});
 
 require __DIR__.'/auth.php';
